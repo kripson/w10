54df6709e5bf26181bcb5702d34baf3a47117640
Samiben1 <samiben498@gmail.com>
2019-09-18 19:20:57 +1000

    added node server and mongodb

--- server/server.js
+++ server/server.js
@@ -1,15 +1,31 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
+const express = require('express');  // Import exprerss.js
+const app = express(); //The app object conventionally denotes the Express application. Create it by 
 const http = require('http').Server(app);
+const bodyParser = require('body-parser'); //create an instance of body-parser
+const cors = require('cors'); //allow Cross site origin requests
+const MongoClient = require('mongodb').MongoClient;  // require MongoClient functionality
+var  ObjectID = require('mongodb').ObjectID; //require ObjectID functionality
 const io = require('socket.io')(http);
 const sockets = require('./socket.js');
-const server = require('./listen.js');
-
-const PORT = 3000;
 
 app.use(cors());
+app.use (bodyParser.json()); //Mounts the specified middleware function
+const url = 'mongodb://localhost:27017';
+MongoClient.connect(url, {poolSize:10,useNewUrlParser: true,useUnifiedTopology: true},function(err, client) {
+    //Callback function code. When we have a connection start the rest of the app.
+    if (err) {return console.log(err)}
+        const dbName = 'mydb';
+        const db = client.db(dbName);
+        sockets.connect(app, io,db);
+        require('./app/api-add.js')(db,app);
+        //require('./routes/api-prodcount.js')(db,app);
+        require('./app/api-validid.js')(db,app);
+        //require('./routes/api-getlist.js')(db,app);
+        require('./app/api-getitem.js')(db,app,ObjectID);
+        require('./app/api-update.js')(db,app,ObjectID);
+        require('./app/api-deleteitem.js')(db,app,ObjectID);
+        
+    //Start the server listening on port 3000. Outputs message to console once server has started.(diagnostic only)
+    require('./listen.js')(http);
 
-sockets.connect(io,PORT)
-
-server.listen(http, PORT);
+});
